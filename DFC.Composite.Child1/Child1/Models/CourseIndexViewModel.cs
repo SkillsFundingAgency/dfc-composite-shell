2deda0e3c61997afaca633deb301a877da5168cc
Ilyas Dhin <ilyasdhin@gmail.com>
2019-06-03 11:50:39 +0100

    Deleted child1 physically as its no longer used

--- DFC.Composite.Child1/Child1/Models/CourseIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Child1.Models
-{
-    public class CourseIndexViewModel
-    {
-        public IEnumerable<Course> Courses { get; set; }
-    }
-}
--- DFC.Composite.Child1/Child1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Child1.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
