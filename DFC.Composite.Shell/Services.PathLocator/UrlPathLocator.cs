49cc010cdb293c91fb2325fc41faf7a677922a4f
Ilyas Dhin <ilyasdhin@gmail.com>
2019-06-04 13:45:14 +0100

    Changed logging level and added logging in PathLocator

--- DFC.Composite.Shell/Services.PathLocator/UrlPathLocator.cs
+++ DFC.Composite.Shell/Services.PathLocator/UrlPathLocator.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace DFC.Composite.Shell.Services.PathLocator
 {
     public class UrlPathLocator : IPathLocator
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogger<UrlPathLocator> _logger;
 
-        public UrlPathLocator(IHttpContextAccessor httpContextAccessor)
+        public UrlPathLocator(IHttpContextAccessor httpContextAccessor, ILogger<UrlPathLocator> logger)
         {
             _httpContextAccessor = httpContextAccessor;
+            _logger = logger;
         }
 
         public string GetPath()
         {
-            return _httpContextAccessor.HttpContext.Request.Path.Value.Replace(@"/", string.Empty);
+            var result = _httpContextAccessor.HttpContext.Request.Path.Value.Replace(@"/", string.Empty);
+            _logger.LogDebug($"PathLocator. Request.Path is {_httpContextAccessor.HttpContext.Request.Path.Value} and path is {result}");
+            return result;
         }
     }
 }
