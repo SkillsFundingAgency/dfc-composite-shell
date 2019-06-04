bd987340899e1105a6b7806444125ddf90277f6a
Ian Crisp <icrisp@program-systems.co.uk>
2019-06-04 16:57:28 +0100

    Fix to http client policy

--- DFC.Composite.Shell/Common/PolicyName.cs
+++ DFC.Composite.Shell/Common/PolicyName.cs
@@ -3,15 +3,8 @@
     public class PolicyName
     {
         public const string HttpRetry = nameof(HttpRetry);
-        //public const string HttpRetryPath = nameof(HttpRetryPath);
-        //public const string HttpRetryRegion = nameof(HttpRetryRegion);
-        //public const string HttpRetryContent = nameof(HttpRetryContent);
-        //public const string HttpRetrySitemap = nameof(HttpRetrySitemap);
 
         public const string HttpCircuitBreaker= nameof(HttpCircuitBreaker);
-        //public const string HttpCircuitBreakerPath = nameof(HttpCircuitBreakerPath);
-        //public const string HttpCircuitBreakerRegion = nameof(HttpCircuitBreakerRegion);
-        //public const string HttpCircuitBreakerContent = nameof(HttpCircuitBreakerContent);
-        //public const string HttpCircuitBreakerSitemap = nameof(HttpCircuitBreakerSitemap);
+
     }
 }
