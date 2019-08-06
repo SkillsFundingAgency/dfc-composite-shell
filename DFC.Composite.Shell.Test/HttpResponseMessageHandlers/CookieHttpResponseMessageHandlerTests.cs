4b80da8290783ce7d973e6cfaa5d95302a2a8688
Ilyas Dhin <ilyasdhin@gmail.com>
2019-08-06 13:23:03 +0100

    Fixed warnings

--- DFC.Composite.Shell.Test/HttpResponseMessageHandlers/CookieHttpResponseMessageHandlerTests.cs
+++ DFC.Composite.Shell.Test/HttpResponseMessageHandlers/CookieHttpResponseMessageHandlerTests.cs
@@ -28,82 +28,90 @@ namespace DFC.Composite.Shell.Test.HttpResponseMessageHandlers
         [Fact]
         public void CanCopyCookieValuesFromChildAppToShellWhenCookieHasSingleValue()
         {
-            //Arrange
-            var path = "path1";
-            A.CallTo(() => pathLocator.GetPath()).Returns(path);
-            var childHttpResponseMessage = new HttpResponseMessage();
-            childHttpResponseMessage.Headers.Add(HeaderNames.SetCookie, "v1=v1value");
-            childHttpResponseMessage.Headers.Add(HeaderNames.Referer, "Referer1=Referer1Value");
+            using (var childHttpResponseMessage = new HttpResponseMessage())
+            {
+                //Arrange
+                var path = "path1";
+                A.CallTo(() => pathLocator.GetPath()).Returns(path);
+                childHttpResponseMessage.Headers.Add(HeaderNames.SetCookie, "v1=v1value");
+                childHttpResponseMessage.Headers.Add(HeaderNames.Referer, "Referer1=Referer1Value");
 
-            //Act
-            cookieHttpResponseMessageHandler.Process(childHttpResponseMessage);
+                //Act
+                cookieHttpResponseMessageHandler.Process(childHttpResponseMessage);
 
-            //Assert
-            var shellResponseHeaders = httpContextAccessor.HttpContext.Response.Headers;
-            var setCookieHeader = shellResponseHeaders[HeaderNames.SetCookie];
-            Assert.Single(setCookieHeader);
-            Assert.StartsWith(path, setCookieHeader[0], StringComparison.OrdinalIgnoreCase);
+                //Assert
+                var shellResponseHeaders = httpContextAccessor.HttpContext.Response.Headers;
+                var setCookieHeader = shellResponseHeaders[HeaderNames.SetCookie];
+                Assert.Single(setCookieHeader);
+                Assert.StartsWith(path, setCookieHeader[0], StringComparison.OrdinalIgnoreCase);
+            }
         }
 
         [Fact]
         public void CanCopyCookieValuesFromChildAppToShellWhenCookieHasMultipleValues()
         {
-            //Arrange
-            var path = "path1";
-            A.CallTo(() => pathLocator.GetPath()).Returns(path);
-            var childHttpResponseMessage = new HttpResponseMessage();
-            childHttpResponseMessage.Headers.Add(HeaderNames.SetCookie, new List<string>() { "v1=value1", "v2=value2" });
-            childHttpResponseMessage.Headers.Add(HeaderNames.Referer, "Referer1=Referer1Value");
+            using (var childHttpResponseMessage = new HttpResponseMessage())
+            {
+                //Arrange
+                var path = "path1";
+                A.CallTo(() => pathLocator.GetPath()).Returns(path);
+                childHttpResponseMessage.Headers.Add(HeaderNames.SetCookie, new List<string>() { "v1=value1", "v2=value2" });
+                childHttpResponseMessage.Headers.Add(HeaderNames.Referer, "Referer1=Referer1Value");
 
-            //Act
-            cookieHttpResponseMessageHandler.Process(childHttpResponseMessage);
+                //Act
+                cookieHttpResponseMessageHandler.Process(childHttpResponseMessage);
 
-            //Assert
-            var shellResponseHeaders = httpContextAccessor.HttpContext.Response.Headers;
-            var setCookieHeader = shellResponseHeaders[HeaderNames.SetCookie];
-            Assert.Equal(2, setCookieHeader.Count);
-            Assert.StartsWith(path, setCookieHeader[0], StringComparison.OrdinalIgnoreCase);
+                //Assert
+                var shellResponseHeaders = httpContextAccessor.HttpContext.Response.Headers;
+                var setCookieHeader = shellResponseHeaders[HeaderNames.SetCookie];
+                Assert.Equal(2, setCookieHeader.Count);
+                Assert.StartsWith(path, setCookieHeader[0], StringComparison.OrdinalIgnoreCase);
+            }
         }
 
         [Fact]
         public void CanCopyCookieValuesCorrectly()
         {
-            //Arrange
-            var path = "path1";
-            A.CallTo(() => pathLocator.GetPath()).Returns(path);
-            var childHttpResponseMessage = new HttpResponseMessage();
-            childHttpResponseMessage.Headers.Add(HeaderNames.SetCookie, new List<string>() { "v1=value1", "v2=value2" });
-            childHttpResponseMessage.Headers.Add(HeaderNames.Referer, "Referer1=Referer1Value");
+            using (var childHttpResponseMessage = new HttpResponseMessage())
+            {
+                //Arrange
+                var path = "path1";
+                A.CallTo(() => pathLocator.GetPath()).Returns(path);
+                childHttpResponseMessage.Headers.Add(HeaderNames.SetCookie, new List<string>() { "v1=value1", "v2=value2" });
+                childHttpResponseMessage.Headers.Add(HeaderNames.Referer, "Referer1=Referer1Value");
 
-            //Act
-            cookieHttpResponseMessageHandler.Process(childHttpResponseMessage);
+                //Act
+                cookieHttpResponseMessageHandler.Process(childHttpResponseMessage);
 
-            //Assert
-            var shellResponseHeaders = httpContextAccessor.HttpContext.Response.Headers;
-            var setCookieHeader = shellResponseHeaders[HeaderNames.SetCookie];
-            Assert.Equal(2, setCookieHeader.Count);
-            Assert.Contains("value1", setCookieHeader[0], StringComparison.OrdinalIgnoreCase);
-            Assert.Contains("value2", setCookieHeader[1], StringComparison.OrdinalIgnoreCase);
+                //Assert
+                var shellResponseHeaders = httpContextAccessor.HttpContext.Response.Headers;
+                var setCookieHeader = shellResponseHeaders[HeaderNames.SetCookie];
+                Assert.Equal(2, setCookieHeader.Count);
+                Assert.Contains("value1", setCookieHeader[0], StringComparison.OrdinalIgnoreCase);
+                Assert.Contains("value2", setCookieHeader[1], StringComparison.OrdinalIgnoreCase);
+            }
         }
 
         [Fact]
         public void CookieValuesAreCopiedToHttpContextItems()
         {
-            //Arrange
-            var path = "path1";
-            A.CallTo(() => pathLocator.GetPath()).Returns(path);
-            var childHttpResponseMessage = new HttpResponseMessage();
-            childHttpResponseMessage.Headers.Add(HeaderNames.SetCookie, new List<string>() { "v1=value1", "v2=value2" });
-            childHttpResponseMessage.Headers.Add(HeaderNames.Referer, "Referer1=Referer1Value");
+            using (var childHttpResponseMessage = new HttpResponseMessage())
+            {
+                //Arrange
+                var path = "path1";
+                A.CallTo(() => pathLocator.GetPath()).Returns(path);
+                childHttpResponseMessage.Headers.Add(HeaderNames.SetCookie, new List<string>() { "v1=value1", "v2=value2" });
+                childHttpResponseMessage.Headers.Add(HeaderNames.Referer, "Referer1=Referer1Value");
 
-            //Act
-            cookieHttpResponseMessageHandler.Process(childHttpResponseMessage);
+                //Act
+                cookieHttpResponseMessageHandler.Process(childHttpResponseMessage);
 
-            //Assert
-            var shellContextItems = httpContextAccessor.HttpContext.Items;
-            Assert.Equal(2, shellContextItems.Count);
-            Assert.Contains("value1", shellContextItems["path1v1"].ToString(), StringComparison.OrdinalIgnoreCase);
-            Assert.Contains("value2", shellContextItems["path1v2"].ToString(), StringComparison.OrdinalIgnoreCase);
+                //Assert
+                var shellContextItems = httpContextAccessor.HttpContext.Items;
+                Assert.Equal(2, shellContextItems.Count);
+                Assert.Contains("value1", shellContextItems["path1v1"].ToString(), StringComparison.OrdinalIgnoreCase);
+                Assert.Contains("value2", shellContextItems["path1v2"].ToString(), StringComparison.OrdinalIgnoreCase);
+            }
         }
     }
 }
