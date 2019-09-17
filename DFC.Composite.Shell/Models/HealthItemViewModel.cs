5a615b7dee6d735e849934b2025bfcb792bb0f82
Ian Crisp <icrisp@program-systems.co.uk>
2019-09-17 12:00:29 +0100

    Added health controller and calls to child apps to get their health

---
+++ DFC.Composite.Shell/Models/HealthItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DFC.Composite.Shell.Models
+{
+    public class HealthItemViewModel
+    {
+        public string Service { get; set; }
+
+        public string Message { get; set; }
+    }
+}
