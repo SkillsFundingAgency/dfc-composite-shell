20a2a434f12bda3a31d00422bf2f26e80122376c
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2020-02-20 10:57:04 +0000

    Resolve ZAP testing CSP header errors using nonce on views

---
+++ DFC.Composite.Shell.Views.UnitTests/Tests/AppInsightsViewComponentTests.cs
@@ -0,0 +1,35 @@
+﻿using DFC.Composite.Shell.Models.Common;
+using DFC.Composite.Shell.ViewComponents;
+using DFC.Composite.Shell.Views.Test.Extensions;
+using FakeItEasy;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DFC.Composite.Shell.Views.Test.Tests
+{
+    public class AppInsightsViewComponentTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("someKey")]
+        public async Task ReturnsAppInsightsKeyWhenItExists(string expectedInsightsKey)
+        {
+            // Arrange
+            var configuration = A.Fake<IConfiguration>();
+            var configSection = A.Fake<IConfigurationSection>();
+            configSection.Value = expectedInsightsKey;
+
+            A.CallTo(() => configuration.GetSection(Constants.ApplicationInsightsInstrumentationKey)).Returns(configSection);
+            var viewComponent = new AppInsightsViewComponent(configuration);
+
+            // Act
+            var result = await viewComponent.InvokeAsync();
+
+            // Assert
+            var viewComponentModel = result.ViewDataModelAs<AppInsightsViewModel>();
+            Assert.Equal(expectedInsightsKey, viewComponentModel.InstrumentationKey);
+        }
+    }
+}
