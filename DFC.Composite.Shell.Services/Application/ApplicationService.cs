9ac2239cffbfb9a66b429c842c3a5cf9152a90ef
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2020-07-28 11:46:41 +0100

    Updated page locations to use a dictionary instead of a list (#203)

--- DFC.Composite.Shell.Services/Application/ApplicationService.cs
+++ DFC.Composite.Shell.Services/Application/ApplicationService.cs
@@ -129,7 +129,7 @@ namespace DFC.Composite.Shell.Services.Application
             var applicationModel = new ApplicationModel();
             var pagesAppRegistrationModel = await appRegistryDataService.GetAppRegistrationModel(appRegistryPathNameForPagesApp).ConfigureAwait(false);
 
-            if (pagesAppRegistrationModel?.Locations != null && pagesAppRegistrationModel.IsOnline && pagesAppRegistrationModel.Locations.Contains("/" + article))
+            if (pagesAppRegistrationModel?.PageLocations != null && pagesAppRegistrationModel.IsOnline && pagesAppRegistrationModel.PageLocations.Values.SelectMany(s => s.Locations).Contains("/" + article))
             {
                 applicationModel.AppRegistrationModel = pagesAppRegistrationModel;
                 applicationModel.Article = article;
