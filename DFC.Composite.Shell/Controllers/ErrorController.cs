40dd920879862d33ffb70b2d7cb7f06897203860
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2020-08-03 12:25:57 +0100

    Bug fixed to prevent too many redirects when unhandled exception cought - now uses interval view instead of redirect /alets/<status-code> (#211)
    Co-authored-by: Muthuramana <muthu@lightningcloudconsultancy.com>

--- DFC.Composite.Shell/Controllers/ErrorController.cs
+++ DFC.Composite.Shell/Controllers/ErrorController.cs
@@ -1,6 +1,11 @@
-﻿using DFC.Composite.Shell.Models.Exceptions;
+﻿using DFC.Composite.Shell.Extensions;
+using DFC.Composite.Shell.Models;
+using DFC.Composite.Shell.Models.Common;
+using DFC.Composite.Shell.Models.Exceptions;
+using DFC.Composite.Shell.Utilities;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Net;
 
@@ -9,10 +14,14 @@ namespace DFC.Composite.Shell.Controllers
     public class ErrorController : Controller
     {
         private readonly ILogger<ApplicationController> logger;
+        private readonly IVersionedFiles versionedFiles;
+        private readonly IConfiguration configuration;
 
-        public ErrorController(ILogger<ApplicationController> logger)
+        public ErrorController(ILogger<ApplicationController> logger, IVersionedFiles versionedFiles, IConfiguration configuration)
         {
             this.logger = logger;
+            this.versionedFiles = versionedFiles;
+            this.configuration = configuration;
         }
 
         [Route("Error")]
@@ -21,15 +30,18 @@ namespace DFC.Composite.Shell.Controllers
             var exceptionPathDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
             var enhancedHttpException = exceptionPathDetails?.Error as EnhancedHttpException;
             var statusCode = enhancedHttpException?.StatusCode ?? HttpStatusCode.InternalServerError;
-            var path = exceptionPathDetails?.Path;
+            var path = exceptionPathDetails?.Path ?? "unknown";
 
-            var errorString = $"{nameof(Error)}: HttpStatusCode: {(int)statusCode} Unhandled error for:{path}: {enhancedHttpException?.Message}";
+            var errorString = $"{nameof(Error)}: HttpStatusCode: {(int)statusCode} Unhandled error for path:{path}: {enhancedHttpException?.Message}";
 
             logger.LogError(exceptionPathDetails?.Error, errorString);
 
             Response.StatusCode = (int)statusCode;
 
-            return Redirect($"/{ApplicationController.AlertPathName}/{(int)statusCode}");
+            var viewModel = versionedFiles.BuildDefaultPageViewModel(configuration);
+            viewModel.LayoutName = $"{Constants.LayoutPrefix}{PageLayout.FullWidth}";
+            viewModel.PageTitle = "Error | National Careers Service";
+            return View(viewModel);
         }
     }
 }
