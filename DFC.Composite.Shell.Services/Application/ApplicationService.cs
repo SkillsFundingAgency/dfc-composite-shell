16c44d4058557491aaa0115a305345e34d7c6d49
Ian Crisp <icrisp@program-systems.co.uk>
2020-09-10 13:26:09 +0100

    Applied changes from story/DFCC-761-update-page-locations-to-dictionary as a hotfix

--- DFC.Composite.Shell.Services/Application/ApplicationService.cs
+++ DFC.Composite.Shell.Services/Application/ApplicationService.cs
@@ -128,8 +128,8 @@ namespace DFC.Composite.Shell.Services.Application
             var article = $"{path}" + (string.IsNullOrWhiteSpace(data) ? string.Empty : $"/{data}");
             var applicationModel = new ApplicationModel();
             var pagesAppRegistrationModel = await appRegistryDataService.GetAppRegistrationModel(appRegistryPathNameForPagesApp).ConfigureAwait(false);
-
-            if (pagesAppRegistrationModel?.Locations != null && pagesAppRegistrationModel.IsOnline && pagesAppRegistrationModel.Locations.Contains("/" + article))
+         
+            if (pagesAppRegistrationModel?.PageLocations != null && pagesAppRegistrationModel.IsOnline && pagesAppRegistrationModel.PageLocations.Values.SelectMany(s => s.Locations).Contains("/" + article))
             {
                 applicationModel.AppRegistrationModel = pagesAppRegistrationModel;
                 applicationModel.Article = article;
