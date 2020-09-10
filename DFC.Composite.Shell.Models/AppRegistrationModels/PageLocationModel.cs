ec47697f281113964b8d0f1fa474fd6a466f0536
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2020-09-10 15:00:36 +0100

    Applied changes from story/DFCC-761-update-page-locations-to-dictionary as a hotfix (#235)

---
+++ DFC.Composite.Shell.Models/AppRegistrationModels/PageLocationModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DFC.Composite.Shell.Models.AppRegistrationModels
+{
+    public class PageLocationModel
+    {
+        public List<string> Locations { get; set; }
+    }
+}
