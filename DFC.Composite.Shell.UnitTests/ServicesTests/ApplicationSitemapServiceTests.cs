1020264cf5fec2b7f5ee4ae4fb7d01c466751792
Ian Crisp <icrisp@program-systems.co.uk>
2021-06-14 09:11:34 +0100

    Updated unit tests

--- DFC.Composite.Shell.UnitTests/ServicesTests/ApplicationSitemapServiceTests.cs
+++ DFC.Composite.Shell.UnitTests/ServicesTests/ApplicationSitemapServiceTests.cs
@@ -3,6 +3,7 @@ using DFC.Composite.Shell.Services.ApplicationSitemap;
 using DFC.Composite.Shell.Services.HttpClientService;
 using DFC.Composite.Shell.Test.ClientHandlers;
 using FakeItEasy;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
 using System.Net;
@@ -14,6 +15,7 @@ namespace DFC.Composite.Shell.Test.ServicesTests
 {
     public class ApplicationSitemapServiceTests
     {
+        private readonly ILogger<ApplicationSitemapService> defaultLogger = A.Fake<ILogger<ApplicationSitemapService>>();
         private readonly HttpClient defaultHttpClient;
 
         public ApplicationSitemapServiceTests()
@@ -37,9 +39,10 @@ namespace DFC.Composite.Shell.Test.ServicesTests
             A.CallTo(() => fakeHttpRequestSender.Send(A<HttpRequestMessage>.Ignored)).Returns(httpResponse);
 
             var fakeHttpMessageHandler = new FakeHttpMessageHandler(fakeHttpRequestSender);
+            var logger = A.Fake<ILogger<ApplicationSitemapService>>();
             var httpClient = new HttpClient(fakeHttpMessageHandler) { BaseAddress = new Uri("http://SomeDummyCDNUrl") };
 
-            var sitemapService = new ApplicationSitemapService(httpClient);
+            var sitemapService = new ApplicationSitemapService(logger, httpClient);
             var model = new ApplicationSitemapModel { BearerToken = "SomeBearerToken" };
 
             var result = await sitemapService.GetAsync(model).ConfigureAwait(false);
@@ -55,22 +58,11 @@ namespace DFC.Composite.Shell.Test.ServicesTests
         [Fact]
         public async Task GetAsyncReturnsNullIfModelIsNull()
         {
-            var sitemapService = new ApplicationSitemapService(defaultHttpClient);
+            var sitemapService = new ApplicationSitemapService(defaultLogger, defaultHttpClient);
 
             var result = await sitemapService.GetAsync(null).ConfigureAwait(false);
 
             Assert.Null(result);
-        }
-
-        [Fact]
-        public async Task GetAsyncReturnsExceptionIfNoSitemapsTextFound()
-        {
-            var sitemapService = new ApplicationSitemapService(defaultHttpClient);
-
-            var model = new ApplicationSitemapModel { BearerToken = "SomeBearerToken" };
-            var exceptionResult = await Assert.ThrowsAsync<InvalidOperationException>(async () => await sitemapService.GetAsync(model).ConfigureAwait(false)).ConfigureAwait(false);
-
-            Assert.StartsWith("An invalid request URI was provided.", exceptionResult.Message, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
