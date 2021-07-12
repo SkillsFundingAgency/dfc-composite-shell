3df8825ebd143ed893592a32bb53656bee18e2ba
ravi-methods <83820221+ravi-methods@users.noreply.github.com>
2021-07-12 08:59:16 +0100

    FT2-661 banner integration (#343)
    Banner integration

---
+++ DFC.Composite.Shell.IntegrationTests/Services/TestBannerService.cs
@@ -0,0 +1,14 @@
+﻿using DFC.Composite.Shell.Services.Banner;
+
+using Microsoft.AspNetCore.Html;
+
+using System.Threading.Tasks;
+
+namespace DFC.Composite.Shell.Integration.Test.Services
+{
+    public class TestBannerService : IBannerService
+    {
+        public Task<HtmlString> GetPageBannersAsync(string path) =>
+            Task.FromResult(new HtmlString("some html"));
+    }
+}
