465ff941f1acfde2d338d3d2f95cdedc27890be7
Ian Crisp <icrisp@program-systems.co.uk>
2021-09-16 09:05:44 +0100

    Updated robots.txt endpoint to use headers to get host name

---
+++ DFC.Composite.Shell/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DFC.Composite.Shell.Extensions
+{
+    [ExcludeFromCodeCoverage]
+    public static class HttpRequestExtensions
+    {
+        public static Uri GetBaseAddress(this HttpRequest request, IUrlHelper? urlHelper = null)
+        {
+            const string xOriginaldProto = "X-Original-Proto";
+            const string xOriginalHost = "X-Original-Host";
+
+            if (request != null)
+            {
+                request.Headers.TryGetValue(xOriginaldProto, out var xOriginalProtocolValue);
+
+                if (string.IsNullOrWhiteSpace(xOriginalProtocolValue))
+                {
+                    xOriginalProtocolValue = request.Scheme ?? Uri.UriSchemeHttp;
+                }
+
+                request.Headers.TryGetValue(xOriginalHost, out var xOriginalHostValue);
+
+                if (string.IsNullOrWhiteSpace(xOriginalHostValue))
+                {
+                    xOriginalHostValue = request.Host.Value;
+                }
+
+                return new Uri($"{xOriginalProtocolValue}://{xOriginalHostValue}{urlHelper?.Content("~")}");
+            }
+
+            return default;
+        }
+    }
+}
