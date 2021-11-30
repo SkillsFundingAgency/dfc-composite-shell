a417458067879a36d12fb12b6f92726602592829
Andy Rose <andy@cyberian.co.uk>
2021-11-30 11:58:29 +0000

    Getting session id from response cookies when not present in request

--- DFC.Composite.Shell/ClientHandlers/CompositeSessionIdDelegatingHandler.cs
+++ DFC.Composite.Shell/ClientHandlers/CompositeSessionIdDelegatingHandler.cs
@@ -1,5 +1,6 @@
 ﻿using DFC.Composite.Shell.Middleware;
 using Microsoft.AspNetCore.Http;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,6 +21,16 @@ namespace DFC.Composite.Shell.ClientHandlers
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             var compositeSessionId = httpContextAccessor?.HttpContext?.Request?.Cookies[CompositeSessionIdMiddleware.NcsSessionCookieName];
+            // For intial requests or if a user has cleared cookies and no request cookie with a session id is present
+            // we need to get it from the response cookie set by the CompositeSessionIdMiddleware else session storage will fail for these requests.
+            if (string.IsNullOrWhiteSpace(compositeSessionId))
+            {
+                var newSessionCookie = httpContextAccessor?.HttpContext?.Response?.GetTypedHeaders().SetCookie?.FirstOrDefault(c => c.Name == CompositeSessionIdMiddleware.NcsSessionCookieName);
+                if (newSessionCookie != null)
+                {
+                    compositeSessionId = newSessionCookie.Value.Value;
+                }
+            }
 
             if (request != null && !request.Headers.Contains(HeaderName) && !string.IsNullOrWhiteSpace(compositeSessionId))
             {
