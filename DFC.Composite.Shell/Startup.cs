9a4bf08300a9c312274e51b36519d1e4259c8265
Stephen Yates <SYatesSoftware@gmail.com>
2022-01-27 07:03:43 +0000

    updated appInsights script

--- DFC.Composite.Shell/Startup.cs
+++ DFC.Composite.Shell/Startup.cs
@@ -99,7 +99,8 @@ namespace DFC.Composite.Shell
                         "https://www.youtube.com",
                         "https://www.google-analytics.com",
                         "https://optimize.google.com",
-                        "https://www.googleoptimize.com"))
+                        "https://www.googleoptimize.com",
+                        "https://js.monitor.azure.com"))
                 .StyleSources(s => s
                     .UnsafeInline()
                     .CustomSources(
