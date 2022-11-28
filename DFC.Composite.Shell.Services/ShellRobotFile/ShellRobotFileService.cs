bd985b69a1bff64160f50a981e91ea4f5a6e5734
LarryOG <43647901+LarryOG@users.noreply.github.com>
2022-11-28 12:55:01 +0000

    fix attempt

--- DFC.Composite.Shell.Services/ShellRobotFile/ShellRobotFileService.cs
+++ DFC.Composite.Shell.Services/ShellRobotFile/ShellRobotFileService.cs
@@ -37,7 +37,7 @@ namespace DFC.Composite.Shell.Services.ShellRobotFile
         private static bool IsDev(string hostname)
         {
             const string dev = "dev-beta.nationalcareersservice.org.uk";
-            return hostname.Contains(dev, System.StringComparison.InvariantCultureIgnoreCase);
+            return hostname.Equals(dev, System.StringComparison.OrdinalIgnoreCase);
         }
 
         private static bool IsPreProduction(string hostname)
@@ -56,7 +56,7 @@ namespace DFC.Composite.Shell.Services.ShellRobotFile
         private string StaticRobotsFilename()
         {
             const string standardRobotsFilename = "StaticRobots.txt";
-            var hostname = httpContextAccessor.HttpContext.Request.Host.ToString();
+            var hostname = httpContextAccessor.HttpContext.Request.Host.Host;
             if (IsDraft(hostname))
             {
                 return standardRobotsFilename;
